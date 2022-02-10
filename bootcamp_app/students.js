4e815ba1edde3db3bc724b9c5dac4cf155a14dfc
ShayFae <ifrahnoor108@gmail.com>
2022-02-09 21:12:58 -0500

    Add query parameter

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,46 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// pool.query(`
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `)
-// .then(res => {
-//   console.log(res.rows);
-// })
-// .catch(err => console.error('query error', err.stack));
-// const fun = function(one, two) {
+pool.connect(() =>{
+  console.log('CONNECTING')
+})
 
-// const text = `
-// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-// FROM students
-// JOIN cohorts ON cohorts.id = cohort_id
-// WHERE cohorts.name LIKE $1
-// LIMIT $2;
-// `;
-// const one = process.argv[2];
-// const two = process.argv[3] || 5;
-// const values = [one, two]
-
-// pool.connect(() =>{
-//   console.log('CONNECTING')
-// })
-
-// pool.query(text, values)
-// .then(res => {
-//   res.rows.forEach(user => {
-//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.one} cohort`);
-//   })
-// });
-
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit]; 
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString =`
 SELECT DISTINCT teachers.name AS name, cohorts.name AS cohort
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name;
-`)
+`;
+
+const value = [process.argv[2]];
+
+pool.query(queryString, value)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.name}`);
